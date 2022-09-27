7a2b32db472820d3aa801f0e2ddcf5ead4eebad8
Raul <jimenezraul1981@gmail.com>
2022-09-26 22:15:09 -0400

    added logo and google login button to login page

--- client/src/features/misc/routes/Landing.tsx
+++ client/src/features/misc/routes/Landing.tsx
@@ -5,10 +5,10 @@ export const Landing = () => {
   return (
     <div className="flex flex-1">
       <div className="w-full mx-auto">
-        <div className="flex flex-wrap justify-center h-full p-3 md:p-10">
+        <div className="flex flex-wrap justify-center h-full p-3 md:p-5">
           <div className="w-full md:w-1/2 flex flex-col items-center md:items-end justify-center">
             <img
-              className="w-3/5 md:w-1/2"
+              className="w-3/5 md:w-10/12 lg:w-1/2"
               src="/assets/img/social-media.png"
               alt=""
             />
@@ -16,7 +16,17 @@ export const Landing = () => {
           <div className="w-full md:w-1/2 flex flex-col md:justify-center bg-gradient-to-b md:bg-gradient-to-r from-slate-900 to-blue-800 rounded-b-2xl md:rounded-r-2xl">
             <div className="p-10 text-center md:text-start">
               <h1 className="text-4xl text-white font-bold">Welcome to</h1>
-              <h2 className="text-4xl text-white font-bold mt-3">Connect <span className="bg-green-500 p-1 rounded-xl border">ME</span></h2>
+              <h2 className="text-4xl text-white font-bold mt-3">
+                Connect{" "}
+                <span className="bg-green-500 p-1 rounded-xl border">ME</span>
+              </h2>
+              <p className="text-xl text-slate-300 mt-5">
+                A social media platform where you can connect with
+                people around the world.
+              </p>
+                <p className="text-slate-300">
+                    Sign up or login to get started
+                </p>
               <button
                 onClick={() => navigate("/login")}
                 className="bg-gradient-to-r from-blue-600 to to-red-500 hover:from-blue-700 hover:to-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-5"
--- client/src/features/users/routes/Login.tsx
+++ client/src/features/users/routes/Login.tsx
@@ -16,9 +16,16 @@ export const Login = () => {
     <div className="flex flex-1 items-center justify-center">
       <form
         onSubmit={handleSubmit}
-        className="bg-slate-800 shadow-md border border-slate-700 rounded-lg px-12 pt-6 pb-8 mb-4"
+        className="bg-slate-800 shadow-md border border-slate-700 rounded-lg px-12 pt-6 pb-8 mb-4 "
       >
-        <h1 className="text-2xl text-center text-slate-300 font-bold mb-5">Login</h1>
+        <h1 className="text-2xl text-center text-slate-300 font-bold mb-3">
+          Login
+        </h1>
+        <img
+          className="h-40 w-40 mx-auto bg-slate-700 rounded-full"
+          src="assets/img/rocket-front-color.png"
+          alt=""
+        />
         <div className="mb-4">
           <label className="block text-slate-300 text-sm font-bold mb-2">
             Email
@@ -58,6 +65,19 @@ export const Login = () => {
           >
             Register
           </Link>
+        </div>
+        <div className="relative flex py-5 items-center">
+          <div className="flex-grow border-t border-gray-400"></div>
+          <span className="flex-shrink mx-4 text-gray-400">OR</span>
+          <div className="flex-grow border-t border-gray-400"></div>
+        </div>
+        <div className="flex items-center justify-center">
+            <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+                Login with Google
+            </button>
         </div>
       </form>
     </div>
