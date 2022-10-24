d9f86efb672d266ef7144c88ece7bc54ffbe7c38
Raul <jimenezraul1981@gmail.com>
2022-10-24 10:28:53 -0400

    added registration form page

--- client/src/features/users/routes/Login.tsx
+++ client/src/features/users/routes/Login.tsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { Button } from "../../../components/CustomButton";
-import { GoogleLoginButton } from "../../../components/GoogleLogin";
-import { validation } from "../../../utils/validation";
-import { useMutation } from "@apollo/client";
-import { LOGIN } from "./api/mutations";
-import { useAppDispatch } from "../../../app/hooks";
-import { user_login, setAccessToken } from "../../../features/users/userSlice";
-import { FormEvent, ChangeEvent } from "./types";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '../../../components/CustomButton';
+import { GoogleLoginButton } from '../../../components/GoogleLogin';
+import { validation } from '../../../utils/validation';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from './api/mutations';
+import { useAppDispatch } from '../../../app/hooks';
+import { user_login, setAccessToken } from '../../../features/users/userSlice';
+import { FormEvent, ChangeEvent } from './types';
 
 export const Login = () => {
   const dispatch = useAppDispatch();
-  const [errors, setErrors] = useState<String>("");
+  const [errors, setErrors] = useState<String>('');
 
   const [login] = useMutation(LOGIN);
 
   const [formState, setFormState] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
     error: {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     },
   });
 
@@ -49,15 +49,15 @@ export const Login = () => {
       }
 
       setFormState({
-        email: "",
-        password: "",
+        email: '',
+        password: '',
         error: {
-          email: "",
-          password: "",
+          email: '',
+          password: '',
         },
       });
-  
-      localStorage.setItem("user", JSON.stringify(user));
+
+      localStorage.setItem('user', JSON.stringify(user));
       dispatch(user_login(user));
       dispatch(setAccessToken(access_token));
 
@@ -74,78 +74,76 @@ export const Login = () => {
       [name]: value,
       error: {
         ...formState.error,
-        [name]: "",
+        [name]: '',
       },
     });
 
-    if (errors) setErrors("");
+    if (errors) setErrors('');
   };
 
   return (
-    <div className="flex flex-1 items-center justify-center">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-slate-800 shadow-md border border-slate-700 rounded-lg px-12 pt-6 pb-8 mb-4 "
-      >
-        <h1 className="text-2xl text-center text-slate-300 font-bold mb-3">
-          Login
-        </h1>
-        <img
-          className="h-40 w-40 mx-auto bg-gradient-to-r from-blue-600 to to-red-600 rounded-full"
-          src="assets/img/rocket-front-color.png"
-          alt=""
-        />
-        <div className="mb-4">
-          <label className="block text-slate-300 text-sm font-bold mb-2">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            value={formState.email}
-            onChange={(e) => handleChange(e)}
-            className="bg-slate-700 shadow appearance-none rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline focus:shadow-outline"
-            placeholder="john@example.com"
+    <div className='flex flex-1 items-center justify-center'>
+      <div className='w-full max-w-md'>
+        <form
+          onSubmit={handleSubmit}
+          className='bg-slate-800 shadow-md border border-slate-700 rounded-lg px-12 pt-6 pb-8 mb-4'
+        >
+          <h1 className='text-2xl text-center text-slate-300 font-bold mb-3'>
+            Login
+          </h1>
+          <img
+            className='h-40 w-40 mx-auto bg-gradient-to-r from-blue-600 to to-red-600 rounded-full'
+            src='assets/img/rocket-front-color.png'
+            alt=''
           />
-          <div className="text-red-500 text-xs">{formState.error.email}</div>
-        </div>
-        <div className="mb-6">
-          <label className="block text-slate-300 text-sm font-bold mb-2">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            value={formState.password}
-            onChange={(e) => handleChange(e)}
-            className="bg-slate-700 shadow appearance-none rounded w-full py-2 px-3 text-gray-200 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="******************"
-          />
-          <div className="text-red-500 text-xs">{formState.error.password}</div>
-        </div>
-        <div className="text-red-500 text-xs">{errors}</div>
-        <div className="flex items-center justify-between">
-          <Button
-            type="submit"
-            name="Login"
-            className="bg-gradient-to-r from-blue-600 to to-red-500 hover:from-blue-700 hover:to-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-5"
-          />
-          <Link
-            to="/register"
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-          >
-            Register
-          </Link>
-        </div>
-        <div className="relative flex py-5 items-center">
-          <div className="flex-grow border-t border-gray-400"></div>
-          <span className="flex-shrink mx-4 text-gray-400">or</span>
-          <div className="flex-grow border-t border-gray-400"></div>
-        </div>
-        <div className="flex items-center justify-center">
-          <GoogleLoginButton setErrors={setErrors} />
-        </div>
-      </form>
+          <div className='mb-4 mt-5'>
+            <input
+              type='email'
+              name='email'
+              value={formState.email}
+              onChange={(e) => handleChange(e)}
+              className='bg-slate-700 shadow appearance-none rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline focus:shadow-outline'
+              placeholder='Email'
+            />
+            <div className='text-red-500 text-xs'>{formState.error.email}</div>
+          </div>
+          <div className='mb-6'>
+            <input
+              type='password'
+              name='password'
+              value={formState.password}
+              onChange={(e) => handleChange(e)}
+              className='bg-slate-700 shadow appearance-none rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline focus:shadow-outline'
+              placeholder='Password'
+            />
+            <div className='text-red-500 text-xs'>
+              {formState.error.password}
+            </div>
+          </div>
+          <div className='text-red-500 text-xs'>{errors}</div>
+          <div className='flex items-center justify-between'>
+            <Button
+              type='submit'
+              name='Login'
+              className='bg-gradient-to-r from-blue-600 to to-red-500 hover:from-blue-700 hover:to-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-5'
+            />
+            <Link
+              to='/register'
+              className='inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800'
+            >
+              Register
+            </Link>
+          </div>
+          <div className='relative flex py-5 items-center'>
+            <div className='flex-grow border-t border-gray-400'></div>
+            <span className='flex-shrink mx-4 text-gray-400'>or</span>
+            <div className='flex-grow border-t border-gray-400'></div>
+          </div>
+          <div className='flex items-center justify-center'>
+            <GoogleLoginButton setErrors={setErrors} />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- client/src/features/users/routes/Register.tsx
+++ client/src/features/users/routes/Register.tsx
@@ -1,7 +1,130 @@
-export const Register = () => {
+import { ChangeEvent, FC, FormEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { GoogleLoginButton } from '../../../components/GoogleLogin';
+import { Button } from '../../../components/CustomButton';
+
+interface formInfo {
+  name: string;
+  type: string;
+  placeholder: string;
+}
+
+type T = {
+  [key: string]: string;
+};
+
+const inputFields: Array<formInfo> = [
+  {
+    name: 'given_name',
+    type: 'text',
+    placeholder: 'First Name',
+  },
+  {
+    name: 'family_name',
+    type: 'text',
+    placeholder: 'Last Name',
+  },
+  {
+    name: 'email',
+    type: 'email',
+    placeholder: 'Email',
+  },
+  {
+    name: 'password',
+    type: 'password',
+    placeholder: 'Password',
+  },
+  {
+    name: 'confirm_password',
+    type: 'password',
+    placeholder: 'Confirm Password',
+  }
+];
+
+export const Register: FC = () => {
+  const [errors, setErrors] = useState<String>('');
+  const [formState, setFormState] = useState<T>({
+    given_name: '',
+    family_name: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+  });
+  const [errorState, setErrorState] = useState<T>({
+    given_name: '',
+    family_name: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+  });
+  
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    console.log('Register');
+  };
+
+  const handleChange = (e: ChangeEvent) => {
+    const { name, value } = e.target as HTMLInputElement;
+    setFormState({ ...formState, [name]: value });
+
+    console.log(formState);
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className='flex flex-1 items-center justify-center my-3'>
+      <div className='w-full max-w-md'>
+        <form
+          onSubmit={handleSubmit}
+          className='bg-slate-800 shadow-md border border-slate-700 rounded-lg px-12 pt-6 pb-8 mb-4'
+        >
+          <h1 className='text-2xl text-center text-slate-300 font-bold mb-3'>
+            Register
+          </h1>
+          <img
+            className='h-40 w-40 mx-auto bg-gradient-to-r from-blue-600 to to-red-600 rounded-full'
+            src='assets/img/rocket-front-color.png'
+            alt=''
+          />
+          <div className='mt-3'>
+            {inputFields.map(({name,type,placeholder}:formInfo) => (
+              <div key={name} className="mb-4">
+            <input
+              type={type}
+              name={name}
+              value={formState[name as keyof typeof formState]}
+              onChange={(e) => handleChange(e)}
+              className='bg-slate-700 shadow appearance-none rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline focus:shadow-outline'
+              placeholder={placeholder}
+            />
+            <div className='text-red-500 text-xs'>{errorState[name as keyof typeof errorState]}</div>
+                </div>
+            ))}
+          </div>
+          <div className='text-red-500 text-xs'>{errors}</div>
+          <div className='flex items-center justify-between'>
+            <Button
+              type='submit'
+              name='Register'
+              className='bg-gradient-to-r from-blue-600 to to-red-500 hover:from-blue-700 hover:to-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-5'
+            />
+            <Link
+              to='/login'
+              className='inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800'
+            >
+              Login
+            </Link>
+          </div>
+          <div className='relative flex py-5 items-center'>
+            <div className='flex-grow border-t border-gray-400'></div>
+            <span className='flex-shrink mx-4 text-gray-400'>or</span>
+            <div className='flex-grow border-t border-gray-400'></div>
+          </div>
+          <div className='flex items-center justify-center'>
+            <GoogleLoginButton setErrors={setErrors} />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
