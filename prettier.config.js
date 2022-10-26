782f43e06da8992fa764554f57e2e959f18e0d1f
Raul <jimenezraul1981@gmail.com>
2022-10-26 13:31:39 -0400

    added prettier config file

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+};
