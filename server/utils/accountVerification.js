34ece56322dcf3cdc3114bf6f6fd6fefeb93be8c
Raul <jimenezraul1981@gmail.com>
2022-10-27 23:48:39 -0400

    fixed a typo in the verification link

--- server/utils/accountVerification.js
+++ server/utils/accountVerification.js
@@ -214,7 +214,7 @@ module.exports = {
                 <tr>
                   <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
                     <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
-                    <p style="margin: 0;"><a href="${process.env.WEBSITE_URL}1/verify?token=${token}" target="_blank">${process.env.WEBSITE_URL}/verify?token=${token}</a></p>
+                    <p style="margin: 0;"><a href="${process.env.WEBSITE_URL}/verify?token=${token}" target="_blank">${process.env.WEBSITE_URL}/verify?token=${token}</a></p>
                   </td>
                 </tr>
                 <!-- end copy -->
