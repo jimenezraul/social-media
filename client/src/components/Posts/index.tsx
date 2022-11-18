cdf893e58feea263353a97783e5993d467334429
Raul <jimenezraul1981@gmail.com>
2022-11-17 21:53:20 -0500

    added modal to confirm post deletion

--- client/src/components/Posts/index.tsx
+++ client/src/components/Posts/index.tsx
@@ -19,6 +19,7 @@ export const Post = ({
   const commentRef = useRef() as React.MutableRefObject<HTMLInputElement>;
   const menuRef = useRef() as React.MutableRefObject<HTMLInputElement>;
   const [isOpen, setIsOpen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const comment = commentCount <= 1 ? 'comment' : 'comments';
   const [likePost] = useMutation(LIKE_POST);
   const [deletePost] = useMutation(DELETE_POST);
@@ -48,6 +49,7 @@ export const Post = ({
           cache.evict({ id: `Post:${_id}` });
         },
       });
+      setOpenModal(false);
     } catch (error) {
       console.log(error);
     }
@@ -81,6 +83,57 @@ export const Post = ({
         isLastEl && 'mb-24 md:mb-4'
       }  border border-slate-700 mb-4 break-inside rounded-lg bg-slate-800 flex flex-col bg-clip-border`}
     >
+      <div
+        className={`${
+          openModal ? 'flex justify-center items-center' : 'hidden'
+        } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 bottom-0 z-50 p-4 w-full md:inset-0 h-modal md:h-full`}
+      >
+        <div className="relative w-full max-w-2xl h-auto">
+          <div className="relative rounded-lg shadow bg-slate-700 border border-slate-500">
+            <div className="flex justify-between items-start p-4 rounded-t border-b border-slate-600">
+              <h3 className="text-xl font-semibold text-slate-100">
+                Are you sure you want to delete this post?
+              </h3>
+              <button
+                type="button"
+                className="text-slate-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                onClick={() => setOpenModal(false)}
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+            </div>
+            <div className="flex justify-end items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+              <button
+                onClick={handleDelete}
+                type="button"
+                className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Delete
+              </button>
+              <button
+                onClick={() => setOpenModal(false)}
+                type="button"
+                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="relative flex p-6 items-center justify-between">
         <div ref={menuRef}>
           {isProfile && (
@@ -94,7 +147,10 @@ export const Post = ({
                   <button className="w-full flex justify-between items-center hover:bg-blue-500 px-3 py-1 rounded">
                     Edit <i className="text-xl fa-solid fa-pen-to-square"></i>
                   </button>
-                  <button onClick={handleDelete} className="w-full flex justify-between items-center hover:bg-red-500 px-3 py-1 rounded">
+                  <button
+                    onClick={() => setOpenModal(true)}
+                    className="w-full flex justify-between items-center hover:bg-red-500 px-3 py-1 rounded"
+                  >
                     Delete <i className="ml-2 text-xl fa-solid fa-trash"></i>
                   </button>
                 </div>
@@ -159,7 +215,9 @@ export const Post = ({
                 return null;
               })}
               {likes.length > 3 && (
-                <span className="absolute text-sm left-16 font-bold text-slate-300 ml-2">+{likes.length - 3} more</span>
+                <span className="absolute text-sm left-16 font-bold text-slate-300 ml-2">
+                  +{likes.length - 3} more
+                </span>
               )}
             </div>
           )}
--- client/src/features/users/routes/FriendProfile.tsx
+++ client/src/features/users/routes/FriendProfile.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useQuery, useLazyQuery } from '@apollo/client';
 import { GET_FRIEND, GET_ME } from '../../../utils/queries';
@@ -6,13 +6,13 @@ import { MeCard } from '../../../components/MeCard';
 import { Post } from '../../../components/Posts';
 
 export const FriendProfile = () => {
+  const navigate = useNavigate();
   const [me, setMe] = useState<User | undefined>(undefined);
   const [friend, setFriend] = useState<User | undefined>(undefined);
   const { id } = useParams<{ id: string }>()!;
-  const [Me, { data: meData, loading: meLoading, error: meError }] =
-    useLazyQuery(GET_ME, {
-      fetchPolicy: 'no-cache',
-    });
+  const [Me, { data: meData, loading: meLoading, error: meError }] = useLazyQuery(GET_ME, {
+    fetchPolicy: 'no-cache',
+  });
 
   const { data, loading, error } = useQuery(GET_FRIEND, {
     variables: { friendId: id },
@@ -35,30 +35,33 @@ export const FriendProfile = () => {
       Me();
     }
 
+    // if the id equals my id, redirect to profile
+    if (meData?.me._id === id) {
+      navigate('/profile');
+    }
+
     if (meLoading) return;
     if (meError) return;
     if (!meData) return;
 
     setMe(meData.me);
-  }, [data, loading, error, Me, meData, meLoading, meError]);
+  }, [data, loading, error, Me, meData, meLoading, meError, id, navigate]);
 
-  const isFriendRequest = me?.friendRequests.find(
-    (friend) => friend._id === id
-  );
+  const isFriendRequest = me?.friendRequests.find((friend) => friend._id === id);
 
   return (
-    <div className='lg:container mx-auto w-full'>
-      <div className='flex flex-wrap justify-center h-full max-h-full overflow-y-scroll sm:overflow-y-hidden no-scrollbar'>
-        <div className='w-full sm:max-w-xs  px-2 mb-4'>
+    <div className="lg:container mx-auto w-full">
+      <div className="flex flex-wrap justify-center h-full max-h-full overflow-y-scroll sm:overflow-y-hidden no-scrollbar">
+        <div className="w-full sm:max-w-xs  px-2 mb-4">
           <MeCard me={friend} inFriendRequest={!!isFriendRequest} />
         </div>
-        <div className='flex flex-col flex-1 w-full sm:max-w-sm md:max-w-lg px-2 h-full sm:overflow-y-scroll no-scrollbar text-white'>
+        <div className="flex flex-col flex-1 w-full sm:max-w-sm md:max-w-lg px-2 h-full sm:overflow-y-scroll no-scrollbar text-white">
           {!isFriendRequest &&
             (friend?.posts.length! > 0 ? (
               friend?.posts.map((post) => <Post key={post._id} {...post} />)
             ) : (
-              <div className='text-center'>
-                <h1 className='text-2xl font-bold'>No Posts</h1>
+              <div className="text-center">
+                <h1 className="text-2xl font-bold">No Posts</h1>
               </div>
             ))}
         </div>
