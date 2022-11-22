074446e752e4032c87121d752d95fa2fcdf697e3
Raul <jimenezraul1981@gmail.com>
2022-11-21 21:48:40 -0500

    added some padding to the login page

--- client/src/features/users/routes/Login.tsx
+++ client/src/features/users/routes/Login.tsx
@@ -83,7 +83,7 @@ export const Login = () => {
   };
 
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center px-2">
       <div className="w-full max-w-md">
         <form
           onSubmit={handleSubmit}
