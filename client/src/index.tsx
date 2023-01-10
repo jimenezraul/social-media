880a944b7172b6bd71b4ab0977be212ae7689606
Raul <jimenezraul1981@gmail.com>
2023-01-10 08:04:54 -0500

    added build for production

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -11,7 +11,7 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <GoogleOAuthProvider clientId="759091763684-s8i5j4sq4fr84mqneo6vaq7de4sdu7hd.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="759091763684-m7nlinpjcjuil9t8qpetf2qfhnhmvvvb.apps.googleusercontent.com">
     <Provider store={store}>
       <App />
     </Provider>
